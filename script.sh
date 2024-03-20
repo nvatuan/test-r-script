21cf79dabef2df0b4fe4233c6caf8a18fdd633a6
Touya (nvatuan) <lambda.nvat@gmail.com>
2024-03-20 22:14:48 +0900

    add shell cmd

--- script.sh
+++ script.sh
@@ -12,7 +12,13 @@ elif [ "$1" = "run" ]; then
   echo "Running the project..."
   docker run --env-file ./.env "${TAG}"
 
-# No valid parameter provided
+elif [ "$1" = "shell" ]; then
+  echo "Start a hanging container and exec shell.."
+  CONTAINER_NAME=r-debug-shell
+  docker run --rm --name $CONTAINER_NAME -d $TAG tail -f /dev/null
+  docker exec -it $CONTAINER_NAME bash
+  docker stop $CONTAINER_NAME
+
 else
   echo "Invalid parameter. Please provide either 'build' or 'run'."
 fi
